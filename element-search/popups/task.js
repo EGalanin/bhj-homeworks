5b2c2ddf54afa35c48e30f78589cf555008293e1
EGalanin <99761893+EGalanin@users.noreply.github.com>
2023-01-11 11:43:55 +0300

    Add files via upload

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,7 +4,7 @@ const successPopup = document.getElementById("modal_success");
 const showSuccess = document.querySelector(".show-success");
 const closePopup = document.querySelectorAll("div.modal__close");
 
-for (i = 0; i < closePopup.length; i++) {
+for (let i = 0; i < closePopup.length; i++) {
   let element = closePopup.item(i);
   element.onclick = close;
     
