cdb4aa569ec45c67b5405ac8174e78861d9b06e2
EGalanin <99761893+EGalanin@users.noreply.github.com>
2023-01-30 09:58:21 +0300

    внес изменения

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,24 @@
+// const reveal = document.querySelectorAll('.reveal');
+
+// reveal.forEach((item) => {
+//     window.addEventListener('scroll', (evt) => {
+//         if (window.innerHeight > item.getBoundingClientRect().top) {
+//             item.classList.add('reveal_active');
+//         }
+//     })
+// })
+
+
 const reveal = document.querySelectorAll('.reveal');
+const { innerHeight } = window;
+const { top } = reveal.getBoundingClientRect();
 
 reveal.forEach((item) => {
     window.addEventListener('scroll', (evt) => {
-        if (window.innerHeight > item.getBoundingClientRect().top) {
-            item.classList.add('reveal_active');
-        }
+        if (top < innerHeight && top > 0) {
+            item.classList.add("reveal_active");
+          } else {
+            item.classList.remove("reveal_active");
+          }
     })
 })
